7a364a457283cef641569363f0e510aeec5ebeb6
Yu-Ta Lu <luyota@corn09.stanford.edu>
2010-10-15 01:02:24 -0700

    A

--- Engine.h
+++ Engine.h
@@ -2,10 +2,12 @@
 #define ENGINE_H
 
 #include <string>
+#include <list>
 
 #include "Ptr.h"
 #include "PtrInterface.h"
 #include "Instance.h"
+
 
 namespace Shipping {
 
@@ -22,10 +24,16 @@ class Port : public Location;
 class Terminal : public Location;
 class Fleet;
 */
+typedef unsigned int U32;
 enum TransportationMode {
     truck, boat, plane    
 };
+
+class Segment;
 class Location : public Fwk::PtrInterface<Location>{
+
+protected:
+    List<Segment> segments_;
 };
 class Customer : public Location {
 };
@@ -33,9 +41,26 @@ class Port : public Location {
 
 };
 class Terminal : public Location {
+public:
+    Terminal (TransportationMode _transMode);
+    TransportationMode transportationMode ();
+    void transportationModeIs (TransportationMode _transMode);
+private:
+    TransportationMode transMode_;
 };
 
 class Segment : public Fwk::PtrInterface<Segment> {
+public:
+    Segment (TransportationMode _transMode);
+    TransportationMode transportationMode ();
+    void transportationModeIs (TransportationMode _transMode);
+private:
+    TransportationMode transMode_;
+    Ptr<Location> source_;
+    U32 length_; // need to be modified
+    Ptr<Segment> returnSegment_;
+    U32 difficulty_;
+    bool expediteSupport_;
 /*  VehicleType mode;
   Location source;
   Distance length;
@@ -44,6 +69,13 @@ class Segment : public Fwk::PtrInterface<Segment> {
   ExpediteSupport expediteSupport;*/
 };
 class Fleet {
+public:
+    Fleet (TransportationMode _transMode);
+    TransportationMode transportationMode ();
+    void transportationModeIs (TransportationMode _transMode);
+private:
+    TransportationMode transMode_;
+    U32 speed, capacity, cost;
 
 };
 
@@ -52,7 +84,7 @@ class Engine {
 public:
   static Ptr<Customer> customerNew();
   static Ptr<Port> portNew ();
-  static Ptr<Terminal> terminalNew ();
+  static Ptr<Terminal> terminalNew (TransportationMode);
   static Ptr<Segment> segmentNew();
   static Ptr<Fleet> fleetNew ();
 
