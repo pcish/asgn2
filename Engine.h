95fd169e0fd65febefe2c7e709f4b8b115d9e1f5
Yu-Ta Lu <luyota@corn12.stanford.edu>
2010-10-21 00:16:40 -0700

    a notifiee try

--- Engine.h
+++ Engine.h
@@ -10,6 +10,8 @@
 using namespace std;
 #include "Ptr.h"
 #include "PtrInterface.h"
+//#include "fwk/BaseNotifiee.h"
+//#include "fwk/NamedInterface.h"
 #include "Instance.h"
 #include "Nominal.h"
 #include "types.h"
@@ -22,10 +24,19 @@ namespace Shipping {
 TODO list:
 1. Add destructor for every class 
 2. Delete segment from Location
-
-
-
 */
+/*
+template <class T>
+class BaseNotifiee {
+    public:
+        BaseNotifiee(T* _notifier) {  notifier_ = _notifier; }
+        void notifierIs (T* _notifier) {
+        }
+    protected:
+        T* notifier_;
+};
+*/
+
 class Location;
 
 class Segment : public Fwk::PtrInterface<Segment> {
@@ -51,8 +62,36 @@ public:
     void difficultyIs (const SegmentDifficultyUnit& _difficulty) { difficulty_ = _difficulty; }
     bool expediteSupport() const { return expediteSupport_; }
     void expediteSupportIs (const bool _expediteSupport) { expediteSupport_ = _expediteSupport; }
+    
+    class Notifiee {
+        public:
+            virtual void onExpediteSupport() {}
+            virtual void onSegment() {}
+            virtual void notifierIs(Segment* _notifier) {
+                if (notifier_ == _notifier)
+                    return;
+                if (notifier_) 
+                    notifier_->notifieeIs(0);
+                notifier_ = _notifier;
+            }
+            static Notifiee* notifieeNew () {
+                Notifiee* n = new Notifiee ();
+                return n;
+            }
+        protected:
+            Segment* notifier_;
+            Notifiee () : notifier_(0) {}
+    };
+    Segment::Notifiee* notifiee() const { return notifiee_; }
+    
+
 protected:
     string name_;
+    Segment::Notifiee* notifiee_;
+    void notifieeIs( Segment::Notifiee* n) const {
+        Segment* me = const_cast<Segment*>(this);
+        me->notifiee_ = n;
+    }
 private:
     Segment (const string& _name, const TransportationMode& _transMode) : name_(_name), transMode_(_transMode) {}
     TransportationMode transMode_;
@@ -64,58 +103,58 @@ private:
 };
 
 class Location : public Fwk::PtrInterface<Location>{
-public:
-    //static Ptr<Location> LocationNew (const string _name) { } 
-    //declared virtual because subclasses may have additional contraints on the type of segment that connects
-    Ptr<Segment> segment (unsigned int index);
-    virtual void segmentIs (Ptr<Segment> seg);
-    virtual void segmentIs (unsigned int index, Ptr<Segment> seg); // assign segment for nth segment
-    //unsigned int segments ();
-    string name() { return name_; }
-protected:
-    Location (const string& _name) : name_(_name) {}
-    vector<Ptr<Segment> > segments_;
-    string name_;
+    public:
+        //static Ptr<Location> LocationNew (const string _name) { } 
+        //declared virtual because subclasses may have additional contraints on the type of segment that connects
+        Ptr<Segment> segment (unsigned int index);
+        virtual void segmentIs (Ptr<Segment> seg);
+        virtual void segmentIs (unsigned int index, Ptr<Segment> seg); // assign segment for nth segment
+        //unsigned int segments ();
+        string name() { return name_; }
+    protected:
+        Location (const string& _name) : name_(_name) {}
+        vector<Ptr<Segment> > segments_;
+        string name_;
 };
 
 class Customer : public Location {
-public:
-    static Ptr<Customer> CustomerNew(const string& _name) { return new Customer(_name); }
-protected:
-    Customer(string _name) : Location(_name) {}
+    public:
+        static Ptr<Customer> CustomerNew(const string& _name) { return new Customer(_name); }
+    protected:
+        Customer(string _name) : Location(_name) {}
 };
 class Port : public Location {
-public:
-    static Ptr<Port> CustomerNew(const string& _name) { return new Port(_name); }
-protected:
-    Port(string _name) : Location(_name) {}
+    public:
+        static Ptr<Port> CustomerNew(const string& _name) { return new Port(_name); }
+    protected:
+        Port(string _name) : Location(_name) {}
 };
 class Terminal : public Location {
-public:
-    static Ptr<Terminal> TerminalNew(const string& _name, Segment::TransportationMode _transMode) { return new Terminal(_name, _transMode); }
-    Segment::TransportationMode transportationMode () { return transMode_; }
-    //a terminal's type should be assigned at the beginning and cannot withstand changing after instantiated
-    //void transportationModeIs (Segment::TransportationMode _transMode);
-    virtual void segmentIs (Ptr<Segment> seg);
-    virtual void segmentIs (unsigned int index, Ptr<Segment> seg);
-protected:
-    Terminal (string _name, Segment::TransportationMode _transMode) : Location(_name), transMode_ (_transMode) {}
-private:
-    Segment::TransportationMode transMode_;
+    public:
+        static Ptr<Terminal> TerminalNew(const string& _name, Segment::TransportationMode _transMode) { return new Terminal(_name, _transMode); }
+        Segment::TransportationMode transportationMode () { return transMode_; }
+        //a terminal's type should be assigned at the beginning and cannot withstand changing after instantiated
+        //void transportationModeIs (Segment::TransportationMode _transMode);
+        virtual void segmentIs (Ptr<Segment> seg);
+        virtual void segmentIs (unsigned int index, Ptr<Segment> seg);
+    protected:
+        Terminal (string _name, Segment::TransportationMode _transMode) : Location(_name), transMode_ (_transMode) {}
+    private:
+        Segment::TransportationMode transMode_;
 };
 
 class Fleet : public Fwk::PtrInterface<Fleet> {
-public:
-    static Ptr<Fleet> instance();
-    
-    Mile speed (const Segment::TransportationMode _mode) { return speed_.find(_mode)->second; }
-    void speedIs (const Segment::TransportationMode _mode, const Mile& v) { speed_.find(_mode)->second = v; }
-    PackageUnit capacity (const Segment::TransportationMode _mode) { return capacity_.find(_mode)->second; }
-    void capacityIs (const Segment::TransportationMode _mode, const PackageUnit& v) { capacity_.find(_mode)->second = v; }
-    USD cost(const Segment::TransportationMode _mode) { return cost_.find(_mode)->second; }
-    void costIs (const Segment::TransportationMode _mode, const USD& v) { cost_.find(_mode)->second = v; }
-    
-private:
+    public:
+        static Ptr<Fleet> instance();
+
+        Mile speed (const Segment::TransportationMode _mode) { return speed_.find(_mode)->second; }
+        void speedIs (const Segment::TransportationMode _mode, const Mile& v) { speed_.find(_mode)->second = v; }
+        PackageUnit capacity (const Segment::TransportationMode _mode) { return capacity_.find(_mode)->second; }
+        void capacityIs (const Segment::TransportationMode _mode, const PackageUnit& v) { capacity_.find(_mode)->second = v; }
+        USD cost(const Segment::TransportationMode _mode) { return cost_.find(_mode)->second; }
+        void costIs (const Segment::TransportationMode _mode, const USD& v) { cost_.find(_mode)->second = v; }
+
+    private:
     Fleet();
     //Segment::TransportationMode transMode_;
     map<Segment::TransportationMode, Mile> speed_;
