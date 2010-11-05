ccb7ec2e0ef2e6740c5c00c73278d71939861662
Paul Chiang <pcish@stanford.edu>
2010-11-05 02:15:11 -0700

    runtests.sh: make clean before make

--- runtests.sh
+++ runtests.sh
@@ -4,6 +4,7 @@ for f in test/group/*.cpp
 do
   echo $f
   cp $f test.cpp
+  make clean -q
   make -q
   ./test.exe > test.out
   diff -y --suppress-common-lines -W 80 test/group/`basename $f .cpp`.refout test.out
