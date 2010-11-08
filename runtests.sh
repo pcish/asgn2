e2dce31734200753b8316d4ae4dcc8333276d249
Paul Chiang <pcish@stanford.edu>
2010-11-08 14:28:29 -0800

    runtests.sh: use .exe on cygwin only

--- runtests.sh
+++ runtests.sh
@@ -7,13 +7,19 @@ else
   folder=group/
 fi 
 
+exename=test
+if uname | grep -iq cygwin
+then
+  exename=test.exe
+fi
+
 for f in testcases/$folder*.cpp
 do
   echo $f
   cp $f test.cpp
   make clean -s
   make -s
-  ./test.exe > test.out
+  ./$exename > test.out
   diff -y --suppress-common-lines -W 80 testcases/$folder`basename $f .cpp`.refout test.out
   rm -f test.out
   echo ----
